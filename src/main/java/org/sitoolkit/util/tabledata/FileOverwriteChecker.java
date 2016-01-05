a076924612eb5b46623f759afb12aaf2b10f18de
Yuichi Kuwahara <yuichi.kuwahara.0@gmail.com>
2016-01-06 06:46:17 +0900

    Windows以外だと例外が発生するため削除

--- src/main/java/org/sitoolkit/util/tabledata/FileOverwriteChecker.java
+++ src/main/java/org/sitoolkit/util/tabledata/FileOverwriteChecker.java
@@ -19,8 +19,6 @@ package org.sitoolkit.util.tabledata;
 import java.io.File;
 
 import javax.swing.JOptionPane;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,13 +41,6 @@ public class FileOverwriteChecker {
     static {
         SELECTION_VALUES = new Object[] { Answer.y.getDescription(), Answer.a.getDescription(),
                 Answer.n.getDescription(), Answer.q.getDescription() };
-
-        try {
-            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-                | UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }
 
     }
 
